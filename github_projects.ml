db64f2d1548294fdac6848e1790e55d9021db807
Baris Metin <baris@zanzibar.inria.fr>
2010-04-19 15:51:36 +0200

    show languages

--- github_projects.ml
+++ github_projects.ml
@@ -1,5 +1,6 @@
 
 open Json_io
+open Json_type
 open Json_type.Browse
 open Http_client.Convenience
 
@@ -12,6 +13,8 @@ let optionspeclist =
     ("-u", Arg.Set_string (github_user), "Github username")
   ]
 
+let api_url = "http://github.com/api/v2/json"
+
 let page_header_templ user =
   let title = user ^ "' github projects" in
   "<html>" ^
@@ -22,36 +25,44 @@ let page_header_templ user =
 
 let page_footer_templ = "</div></body></html>"
 
-let project_templ name url desc = 
-  "<a href=" ^ url ^ " class=\"project\">" ^ "<h2>" ^ name ^ "</h2>" ^ "<p>" ^ desc ^ "</p>" ^ "</a>"
+let project_templ name url desc langs = 
+  "<a href=" ^ url ^ " class=\"project\">" ^ 
+  "<h2>" ^ name ^ "</h2>" ^ "<p>" ^ desc ^ "</p>" ^ 
+  "<p class=\"lang\">" ^ langs ^ "</p></a>"
 
 let get_field_from json name =
   List.hd
     (List.map (fun (x,y) -> string y)
        (List.filter (fun (x,y) -> x = name) (objekt json)))
 
-let output_repo out repo =
+let repo_langs user repo =
+  let lang_url = Neturl.join_path [api_url; "repos/show"; user; repo; "languages"] in
+  let lang_json = http_get lang_url in
+  let tbl = make_table (objekt (json_of_string lang_json)) in
+  field tbl "languages"
+
+let output_repo out user repo =
   let name = get_field_from repo "name" in
   let url = get_field_from repo "url" in
   let description = get_field_from repo "description" in
-  output_string out (project_templ name url description)
+  let langs = string_of_json (repo_langs user name) in
+  output_string out (project_templ name url description langs)
 
-let projects_page user = 
-  let api_url = "http://github.com/api/v2/json" in
+let output_repos user = 
   let repos_url = Neturl.join_path [api_url; "repos/show"; user] in
   let repos_json = http_get repos_url in
   let tbl = make_table (objekt (json_of_string repos_json)) in
   let repos = array (field tbl "repositories") in
   let out = open_out "projects.html" in
-  let rec output_repos repos = 
+  let rec output_repos' repos = 
     match repos with
       [] -> ()
-    | r::repos' -> (output_repo out r; (output_repos repos')) in
+    | r::repos' -> (output_repo out user r; (output_repos' repos')) in
 
   output_string out (page_header_templ user);
-  output_repos repos;
+  output_repos' repos;
   output_string out page_footer_templ
 
 let _ =
   Arg.parse optionspeclist (fun x -> ()) usage_msg;
-  projects_page !github_user
+  output_repos !github_user
